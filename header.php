6cb1b64d6b549fa4aa44c07d7f4c1fa2cfe67f28
zenyhoj <joe.balingit@gmail.com>
2016-11-14 16:45:05 +0800

    added fontawesome icons and edit navs

--- header.php
+++ header.php
@@ -5,35 +5,59 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="bootstrap/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" media="screen">
 <link href="bootstrap/css/style.css" rel="stylesheet" media="screen">
 <title>HRIS</title>
-
 </head>
 
 <body>
-    <header>
-<div class="navbar navbar-default navbar-static-top" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <!--Toggle PNavigation -->
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-
-            <a class="navbar-brand" href="#">BWD | Human Resource Information System</a>
-        </div>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><i class="glyphicon glyphicon-lock"></i>&nbsp;Welcome | Admin</a></li>
-
-
-            </ul>
-        </div>
+<!-- NAVBAR DEFAULT -->
+<nav class="navbar navbar-default navbar-static-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+        <a class="navbar-brand" href="#"><i class="fa fa-users" aria-hidden="true"></i>
+          BWD-Human Resource Information System </a>
     </div>
-</div>
-  </header>
 
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Dashboard<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Settings</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">PIM<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Job Titles</a></li>
+            <li><a href="#">Departments</a></li>
+            <li><a href="#">Eligibilities</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>
+Register</a></li>
+        <li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i>
+Sign In</a></li>
+
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 </body>
